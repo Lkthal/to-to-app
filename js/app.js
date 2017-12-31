a5bf18331a09490bef48e842be51860160fa94ea
Thal Syhabout <tsyhabout@gmail.com>
2017-12-31 14:43:16 -0800

    revision assignment 28

---
+++ js/app.js
@@ -0,0 +1,93 @@
+function onReady(){
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+  let id = 0;
+
+
+function createNewToDo(){
+
+  if(!newToDoText.value){return;}
+  toDos.push({
+    title: newToDoText.value,
+    complete: false,
+    id: id.value;
+  });
+
+  id+=1;
+
+  newToDoText.value= '';
+
+  renderTheUI();
+}
+
+function renderTheUI(){
+  const toDoList = document.getElementById('toDoList');
+
+  toDoList.textContent = '';
+
+
+
+  toDos.forEach(function(todo){
+    const newTodo = document.createElement('li');
+    const button = document.createElement("button");
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    const title = document.createElement('span');
+    newLi.textContent = toDo.title;
+
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
+    newLi.appendChild(button);
+
+  });
+}
+
+  addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
+      // get the text
+      let title = newToDoText.value;
+
+      // create a new li
+      let newLi = document.createElement('li');
+
+      // create a new input
+      let checkbox = document.createElement('input');
+
+      // set the input's type to checkbox
+      checkbox.type = "checkbox";
+
+      // set the title
+      newLi.textContent = title;
+
+      // attach the checkbox to the li
+      newLi.appendChild(checkbox);
+
+      // attach the li to the ul
+      toDoList.appendChild(newLi);
+
+      //empty the input
+      newToDoText.value = '';
+
+
+  });
+
+function deleteToDo(id){
+  toDos = toDos.filter( item => item.id !== id);
+}
+
+button.addEventListener ("click", event => {
+deleteToDo(toDo.id);
+renderTheUI();
+});
+//adding UI
+renderTheUI();
+}
+
+window.onload = function () {
+  alert("The window has loaded!");
+onReady();
+};
